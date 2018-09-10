78766484b9573d1e1967612661a85df98f1cca14
Chenyao2333 <louchenyao@gmail.com>
2018-09-10 13:59:11 +0800

    [minor]

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd src/search
 make
 cd ../..
 
-rm -f test.log
+rm -f test.*
 
 #blastpgp -i cullpdb25/aa/3aa0a.fa -d database/cullpdb2 -o test.log
 # if high-quality hits found, no need to continue
