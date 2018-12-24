c5bdd94faccf4d5b7c648ee6dbd190f8a693efc9
Chenyao2333 <louchenyao@gmail.com>
2018-12-24 16:02:21 +0800

    [upd] remove explicitly convert pdb to dssp

--- test.py
+++ test.py
@@ -11,12 +11,8 @@ from contactlib.data_manger import asset_path
 e = Encoder()
 s = Searcher()
 
-# generate FASTA and DSSP files, given a PDB file
-convertPDB(asset_path("3aa0a.pdb"))
-
 # build ContactLib fingerprint file
 e.encode(asset_path("3aa0a.pdb"), "3aa0a.cl")
 
 # scan ContactLib for homologous proteins, and store the similarity scores
 s.search("3aa0a.cl", "output.txt")
-
