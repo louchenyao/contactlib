be9b5cac7a872dfa1240750c42d32d2c2ebf5103
Chenyao2333 <louchenyao@gmail.com>
2018-12-28 21:27:51 +0800

    [fix] we can't import contactlib when we are installing. (temporal sol)

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ import os
 from distutils.core import setup
 
 os.environ["PYTHONPATH"] = os.path.dirname(os.path.realpath(__file__))
-from contactlib import __version__
-
-print(__version__)
 
 install_requires = ["requests", "biopython", "numpy", "scipy", "scikit-learn"]
 
@@ -16,7 +13,7 @@ except ImportError:
     install_requires.append("tensorflow")
 
 setup(name='contactlib',
-      version=__version__,
+      version="2.1.4",
       description='Tool for searching proteins through structural fingerprints.',
       url='https://github.com/Chenyao2333/contactlib',
       packages=['contactlib', 'contactlib.search'],
